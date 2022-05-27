d7a10ae6b6a56aeba72ce7bf137e42c022b49264
Rodrigo Moran <rmoran@ipponusa.com>
2022-05-27 09:10:22 -0400

    Create directories before updating the PATH

--- go.sh
+++ go.sh
@@ -6,15 +6,15 @@ set GO_DIR ".go"
 set -Ux GOPATH "$HOME/$GO_DIR"
 set -Ux GOROOT (brew --prefix golang)"/libexec"
 
+# Creating Go directories if they don't already exist
+mkdir -p $GOPATH/bin $GOPATH/src $GOPATH/pkg $GOROOT/bin
+
 # Set Path
 fish_add_path $GOPATH"/bin"
 fish_add_path $GOROOT"/bin"
 
-# Create Go directory
-mkdir -p $GOPATH
-
 # Install Go-Lang (if not already installed)
-if ! [ -x "$(command -v go)" ];
+if ! [ -x "$(command -v go)" ]
 then
     echo "Installing Go Lang";
     brew install go;
