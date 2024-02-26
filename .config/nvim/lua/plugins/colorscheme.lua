5219b4e42df01fa77327fb757d2ffdacff4030c8
Rodrigo Moran <rigo.moran0@gmail.com>
2024-02-26 10:05:35 -0500

    fix: improve light/dark theme switching

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,15 +3,16 @@ return {
     -- Theme inspired by Hyper Term
     'connorholyday/vim-snazzy',
     priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'snazzy'
-    end,
+    -- config = function()
+    --   vim.cmd.colorscheme 'snazzy'
+    -- end,
   },
   {
     'catppuccin/nvim',
     name = 'catppuccin',
     priority = 1000,
     config = function()
+        vim.cmd.colorscheme 'snazzy'
         if os.getenv 'theme' == 'light' then
             local status, _ = pcall(vim.cmd, 'colorscheme catppuccin-latte')
             if not status then
