0d274226b17f647f14b9f9369aba9b0846caeb0b
Rodrigo Moran <rigo.moran0@gmail.com>
2024-04-26 02:22:25 -0400

    add function to configure pyvenv

--- .config/fish/functions/pyv.fish
+++ .config/fish/functions/pyv.fish
@@ -1,13 +1,34 @@
-function pyv --wraps='python3 -m venv .venv --prompt dev' --description 'alias pyv=python3 -m venv .venv --prompt dev'
-  set name $argv
+function pyv --description 'Enable Python Venv or create a new one'
+  # if an argument is provided then create a new venv
+  set -l name $argv
   if test -z "$name"
     set name "dev"
   end
 
-  echo "Creating Python Virtual Env for $name"
-  python3 -m venv .venv --prompt $name
-  
-  set venv_path .venv/bin/activate.fish
-  echo "Enabling Virtual Env at $venv_path"
-  source $venv_path 
+  if test -d ".venv"
+    info "Python Virtual Env already exists"
+    einfo "Enabling Python Virtual Env at .venv/"
+    # echo "enabling python virtual env at .venv/"
+    source .venv/bin/activate.fish
+  else
+    info "Python Virtual Env does not exist"
+    echo "Creating Python Virtual Env for $name"
+    python3 -m venv .venv --prompt $name
+    echo "Enabling Python Virtual Env"
+    source .venv/bin/activate.fish
+  end
 end
+# function pyv --wraps='python3 -m venv .venv --prompt dev' --description 'alias pyv=python3 -m venv .venv --prompt dev'
+#   # If .venv exists then source it
+#   set name $argv
+#   if test -z "$name"
+#     set name "dev"
+#   end
+#
+#   echo "Creating Python Virtual Env for $name"
+#   python3 -m venv .venv --prompt $name
+#   
+#   set venv_path .venv/bin/activate.fish
+#   echo "Enabling Virtual Env at $venv_path"
+#   source $venv_path 
+# end
