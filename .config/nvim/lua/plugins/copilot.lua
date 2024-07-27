bae41ce351f0b0032e85a076408811571a29f825
Rodrigo Moran <rigo.moran0@gmail.com>
2024-07-27 00:21:55 -0300

    comment out copilot plugins

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,19 +1,20 @@
-return {
-  {
-    'zbirenbaum/copilot.lua',
-    cmd = 'Copilot',
-    event = 'InsertEnter',
-    config = function()
-      require('copilot').setup {}
-    end,
-  },
-  {
-    'zbirenbaum/copilot-cmp',
-    config = function()
-      require('copilot_cmp').setup()
-    end,
-  },
-}
+return {}
+-- return {
+--   {
+--     'zbirenbaum/copilot.lua',
+--     cmd = 'Copilot',
+--     event = 'InsertEnter',
+--     config = function()
+--       require('copilot').setup {}
+--     end,
+--   },
+--   {
+--     'zbirenbaum/copilot-cmp',
+--     config = function()
+--       require('copilot_cmp').setup()
+--     end,
+--   },
+-- }
 -- return {
 --   'github/copilot.vim',
 --   opts = {
