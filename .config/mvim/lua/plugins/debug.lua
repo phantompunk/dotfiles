90112e38fe9e7fbb23af90baf2a9a883f0eaad95
Rodrigo Moran <rigo.moran0@gmail.com>
2025-01-27 11:13:45 -0500

    rename testvim to mvim (minimal)

--- .config/mvim/lua/plugins/debug.lua
+++ .config/mvim/lua/plugins/debug.lua
@@ -1,20 +1,11 @@
 -- debug.lua
---
--- Shows how to use the DAP plugin to debug your code.
---
--- Primarily focused on configuring the debugger for Go, but can
--- be extended to other languages as well. That's why it's called
--- kickstart.nvim and not kitchen-sink.nvim ;)
-if true then
-  return {}
-end
+-- if true then
+--   return {}
+-- end
 
 return {
-  -- NOTE: Yes, you can install new plugins here!
   "mfussenegger/nvim-dap",
-  -- NOTE: And you can specify dependencies as well
   dependencies = {
-    -- Creates a beautiful debugger UI
     "rcarriga/nvim-dap-ui",
 
     -- Required dependency for nvim-dap-ui
@@ -30,42 +21,42 @@ return {
   keys = {
     -- Basic debugging keymaps, feel free to change to your liking!
     {
-      "<F5>",
+      "<leader>dC",
       function()
         require("dap").continue()
       end,
       desc = "Debug: Start/Continue",
     },
     {
-      "<F1>",
+      "<leader>di",
       function()
         require("dap").step_into()
       end,
       desc = "Debug: Step Into",
     },
     {
-      "<F2>",
+      "<leader>do",
       function()
         require("dap").step_over()
       end,
       desc = "Debug: Step Over",
     },
     {
-      "<F3>",
+      "<leader>dO",
       function()
         require("dap").step_out()
       end,
       desc = "Debug: Step Out",
     },
     {
-      "<leader>b",
+      "<leader>db",
       function()
         require("dap").toggle_breakpoint()
       end,
       desc = "Debug: Toggle Breakpoint",
     },
     {
-      "<leader>B",
+      "<leader>dB",
       function()
         require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
       end,
@@ -73,7 +64,7 @@ return {
     },
     -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
     {
-      "<F7>",
+      "<leader>du",
       function()
         require("dapui").toggle()
       end,
