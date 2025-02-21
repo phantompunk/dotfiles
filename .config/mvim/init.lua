13ac528b59e13eb83c46f95ece423f2580e1ff19
Rodrigo Moran <rigo.moran0@gmail.com>
2025-02-21 10:41:16 -0500

    fix: light theme toggle

--- .config/mvim/init.lua
+++ .config/mvim/init.lua
@@ -2,8 +2,9 @@ require("keymaps")
 require("options")
 require("config.lazy")
 
+print("Theme", vim.env.theme)
 -- Enable light/dark theme toggle
-local theme = vim.env.THEME == "light" and "catppuccin-latte" or "snazzy"
+local theme = vim.env.theme == "light" and "catppuccin-latte" or "snazzy"
 local ok, _ = pcall(vim.cmd, "colorscheme " .. theme)
 if not ok then
   vim.notify("Failed to load colorscheme: " .. theme, vim.log.levels.WARN)
