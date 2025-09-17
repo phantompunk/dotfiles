d72d870114b426ab024ac64f6698c9cc56aa7213
Rodrigo Moran <rigo.moran0@gmail.com>
2025-09-17 13:55:55 -0400

    fix: neotest config and add copilot toggle

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,3 +1,19 @@
 return {
   "github/copilot.vim",
+  config = function()
+    -- Your existing Copilot config here
+    vim.g.copilot_enabled = false
+    -- Add the toggle keymap
+    vim.keymap.set("n", "<leader>cp", function()
+      if vim.g.copilot_enabled == false then
+        vim.cmd("Copilot enable")
+        vim.g.copilot_enabled = true
+        vim.notify("Copilot enabled", vim.log.levels.INFO)
+      else
+        vim.cmd("Copilot disable")
+        vim.g.copilot_enabled = false
+        vim.notify("Copilot disabled", vim.log.levels.INFO)
+      end
+    end, { desc = "Toggle Copilot" })
+  end,
 }
