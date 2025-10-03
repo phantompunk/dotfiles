fd69a55a1c9ddc0dcf27254eb51721409faf5cfc
Rodrigo Moran <rigo.moran0@gmail.com>
2025-10-03 14:07:23 -0400

    fix: code folding with ufo

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -16,6 +16,46 @@ return {
     },
   },
 
+  -- Improved Folding
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = { "kevinhwang91/promise-async" },
+    event = "BufRead",
+    keys = {
+      {
+        "zR",
+        function()
+          require("ufo").openAllFolds()
+        end,
+      },
+      {
+        "zM",
+        function()
+          require("ufo").closeAllFolds()
+        end,
+      },
+      {
+        "K",
+        function()
+          local winid = require("ufo").peekFoldedLinesUnderCursor()
+          if not winid then
+            vim.lsp.buf.hover()
+          end
+        end,
+      },
+    },
+    config = function()
+      vim.o.foldcolumn = "0"
+      vim.o.foldlevel = 99
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+      vim.o.foldtext = ""
+      vim.api.nvim_set_hl(0, "Folded", { link = "Normal" })
+      require("ufo").setup({
+        close_fold_kinds = { "imports" },
+      })
+    end,
+  },
   -- Main LSP Configuration
   {
     "neovim/nvim-lspconfig",
