3e530ebf11f0da031b47a37db8445f1ef2239a2c
Rodrigo Moran <rigo.moran0@gmail.com>
2025-12-10 15:00:10 -0500

    fix: toggle for copilot

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -2,18 +2,15 @@ return {
 	"github/copilot.vim",
 	event = "InsertEnter",
 	config = function()
-		vim.g.copilot_enabled = false
-
 		-- Add the toggle keymap
 		vim.keymap.set("n", "<leader>cp", function()
-			vim.g.copilot_enabled = not vim.g.copilot_enabled
 
-			if vim.g.copilot_enabled then
-				vim.cmd("Copilot enable")
-				vim.notify("Copilot enabled", vim.log.levels.INFO)
-			else
+			if vim.fn["copilot#Enabled"]() == 1 then
 				vim.cmd("Copilot disable")
 				vim.notify("Copilot disabled", vim.log.levels.WARN)
+			else
+				vim.cmd("Copilot enable")
+				vim.notify("Copilot enabled", vim.log.levels.INFO)
 			end
 		end, { desc = "Toggle Copilot" })
 	end,
